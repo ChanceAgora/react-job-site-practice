8f7dfd3cbe3a17048e45d5b29a6f9da66bb179ff
Chance Agorastos <cagorastos127@gmail.com>
2025-10-26 23:32:39 -0500

    Correct File Names and File Imports

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 function JobListing({job}) {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -29,7 +30,7 @@ function JobListing({job}) {
     
           <div className="flex flex-col lg:flex-row justify-between mb-4">
             <div className="text-orange-700 mb-3">
-              <i className="fa-solid fa-location-dot text-lg"></i>{job.location}</div>
+              <FaMapMarker className="inline text-lg mb-1"></FaMapMarker>{job.location}</div>
             <a
               href={`/job/${job.id}`}
               className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
