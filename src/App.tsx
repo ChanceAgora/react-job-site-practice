750c45fd1d9d4bdf7a25bba7505bfffc90b7b692
Chance Agorastos <cagorastos127@gmail.com>
2025-10-28 21:48:13 -0500

    BROKEN: Separate HomePage into Pages Directory
    Separate the HomePage into the pages directory.
    
    It is broken in its current state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Outlet} from 'react-router-dom';
-import NavBar from "./components/NavBar.tsx";
-import Hero from "./components/Hero.tsx";
-import HomeCards from "./components/HomeCards.tsx";
-import JobListings from "./components/JobListings.tsx";
-import ViewAllJobs from "./components/ViewAllJobs.tsx";
+import { createBrowserRouter,
+         createRoutesFromElements,
+         RouterProvider,
+         Outlet,
+       } from 'react-router-dom';
+import NavBar from './components/NavBar.tsx';
+import HomePage from './pages/HomePage.tsx';
 
-const Root = () => <Outlet />;
+const Root = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     children: [
-      { index: true, element: <h1 className='text-[50px]'>TESTING</h1> },
+      { index: true, element: <HomePage /> },
   ]},
 ]);
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero.tsx';
+import HomeCards from '../components/HomeCards.tsx';
+import JobListings from '../components/JobListings.tsx';
+import ViewAllJobs from '../components/ViewAllJobs.tsx';
+
+function HomePage {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+}
+
+export default HomePage;
