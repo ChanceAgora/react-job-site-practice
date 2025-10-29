f8382cf6e5ffc8d7f8611061e64a02e273da209d
Chance Agorastos <cagorastos127@gmail.com>
2025-10-28 19:57:50 -0500

    Create Basic Route Outline in App.tsx
    Create the basic route outline in the src/App.tsx file.
    
    Initialize a Root element that returns an Outlet. Add a basic test to its children to ensure it is working properly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Outlet} from 'react-router-dom';
 import NavBar from "./components/NavBar.tsx";
 import Hero from "./components/Hero.tsx";
 import HomeCards from "./components/HomeCards.tsx";
 import JobListings from "./components/JobListings.tsx";
 import ViewAllJobs from "./components/ViewAllJobs.tsx";
 
+const Root = () => <Outlet />;
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { index: true, element: <h1 className='text-[50px]'>TESTING</h1> },
+  ]},
+]);
+
 function App() {
+
+  return <RouterProvider router={router} />
 
   return (
     <>
