5b78c9e8098912e2b2270701d85c96de3f9fc19c
Chance Agorastos <cagorastos127@gmail.com>
2025-10-28 21:56:32 -0500

    Fix Syntax Error in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import HomeCards from '../components/HomeCards.tsx';
 import JobListings from '../components/JobListings.tsx';
 import ViewAllJobs from '../components/ViewAllJobs.tsx';
 
-function HomePage {
+function HomePage() {
   return (
     <>
       <Hero />
